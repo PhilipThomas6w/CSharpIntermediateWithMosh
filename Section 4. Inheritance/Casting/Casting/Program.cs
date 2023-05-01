49db63e37abce205191113aa3adb427448b5316a
Philip Thomas <110606536+PhilipThomas6w@users.noreply.github.com>
2023-05-01 17:25:35 +0100

    Added Inheritance.Casting.

---
+++ Section 4. Inheritance/Casting/Casting/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+
+namespace Casting;
+
+public class Program
+{
+    static void Main()
+    {
+        #region UpCasting
+
+        //Text text = new Text();
+        //Shape shape = text;
+
+        //text.Width = 200;
+        //shape.Width = 100;
+
+        //Console.WriteLine(text.Width);
+
+        //StreamReader reader = new StreamReader(new MemoryStream());
+
+        //var list = new ArrayList();
+        //list.Add(1);
+        //list.Add("Luke");
+        //list.Add(new Text());
+
+        //var anotherList = new List<Shape>();
+
+        #endregion
+
+        Shape shape = new Text();
+
+        Text text = (Text)shape;    // downcast
+
+    }
+}
