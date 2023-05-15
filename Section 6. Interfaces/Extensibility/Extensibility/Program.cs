22674636a6ba888751729a7d037dd9d8c7a493ee
Philip Thomas <110606536+PhilipThomas6w@users.noreply.github.com>
2023-05-15 01:13:13 +0100

    Added Interfaces.Polymorphism

--- Section 6. Interfaces/Extensibility/Extensibility/Program.cs
+++ Section 6. Interfaces/Extensibility/Extensibility/Program.cs
@@ -4,7 +4,7 @@ public class Program
 {
     static void Main()
     {
-        var dBMigrator = new DbMigrator(new ConsoleLogger());
+        var dBMigrator = new DbMigrator(new FileLogger("F:\\Repos\\C# Learning Path\\CSharpIntermediateWithMosh\\Section 6. Interfaces\\Extensibility\\Logs\\log.txt"));
         dBMigrator.Migrate();
     }
 }
