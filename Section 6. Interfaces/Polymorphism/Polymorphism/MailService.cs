9d3e15481d23f29c4e682ec4d0375383168bcab7
Philip Thomas <110606536+PhilipThomas6w@users.noreply.github.com>
2023-05-15 01:56:43 +0100

    Added untracked files

---
+++ Section 6. Interfaces/Polymorphism/Polymorphism/MailService.cs
@@ -0,0 +1,9 @@
+﻿namespace Polymorphism;
+
+public class MailService
+{
+    public void Send(Mail mail)
+    {
+        Console.WriteLine("Sending email...");
+    }
+}
---
+++ Section 6. Interfaces/Polymorphism/Polymorphism/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Net.Mail;
+
+namespace Polymorphism;
+
+public class Program
+{
+    static void Main()
+    {
+        var encoder = new VideoEncoder();
+        encoder.Encode(new Video());
+    }
+}
